5406b3b5a435d2376c2eb7e761dc442c08e7ff87
ckendrick-mqu <christopher.kendrick1@students.mq.edu.au>
2026-03-29 16:15:52 +1100

    Changed quotes

--- js/constants.js
+++ js/constants.js
@@ -5,7 +5,7 @@ export const SCRAMBLE_DURATION = 800;
 export const FLIP_DURATION = 300;
 export const STAGGER_DELAY = 25;
 export const TOTAL_TRANSITION = 3800;
-export const MESSAGE_INTERVAL = 4000;
+export const MESSAGE_INTERVAL = 12000;
 
 export const CHARSET = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789.,-!?\'/: ';
 
@@ -22,44 +22,352 @@ export const ACCENT_COLORS = [
 export const MESSAGES = [
   [
     '',
-    'GOD IS IN',
-    'THE DETAILS .',
-    '- LUDWIG MIES',
+    'BRICKS MAKE ME',
+    'SNIFF',
+    '- JACOB',
     ''
   ],
   [
     '',
-    'STAY HUNGRY',
-    'STAY FOOLISH',
-    '- STEVE JOBS',
+    'LISTEN TO THE ROUTER',
+    'CALLING THE MODEM',
+    'WHITE',
+    '- WREN'
+  ],
+  [
+    'MY BODY IS A',
+    'MACHINE THAT TURNS',
+    'OATMEAL INTO POO',
+    '- WREN',
     ''
   ],
   [
     '',
-    'GOOD DESIGN IS',
-    'GOOD BUSINESS',
-    '- THOMAS WATSON',
+    'EVERYONE IS A',
+    'WORK OF ART',
+    '- JACOB',
     ''
   ],
   [
     '',
-    'LESS IS MORE',
+    'REAL QUESTION IS,',
+    'WHY IS THE RAIN',
+    'YELLOW?',
+    '- WREN'
+  ],
+  [
+    'I WAS GONNA MAKE',
+    'A JOKE BUT YOUR',
+    'MOTHER IS SUCH A',
+    'NICE LADY',
+    '- WREN'
+  ],
+  [
     '',
-    '- MIES VAN DER ROHE',
+    'IF YOU FINGER IT',
+    'ENOUGH IT MIGHT',
+    'SPRINKLE',
+    '- ED'
+  ],
+  [
+    'I MEAN IM SURE',
+    'THERES SOME DEAD GUY',
+    'WE CAN EAT...',
+    '- JACOB',
     ''
   ],
   [
     '',
-    'MAKE IT SIMPLE',
-    'BUT SIGNIFICANT',
-    '- DON DRAPER',
+    'WE ARE NO STRANGERS',
+    'TO POO. EVERYONE',
+    'HAS TO DO IT.',
+    '- ED'
+  ],
+  [
+    '',
+    'YOUVE JUST BUGGED',
+    'YOUR LAST BUGGAR',
+    '- JACOB',
     ''
   ],
   [
     '',
-    'HAVE NO FEAR OF',
-    'PERFECTION',
-    '- SALVADOR DALI',
+    'ITS GOOPING',
+    'TIIIIIME',
+    '- ED',
+    ''
+  ],
+  [
+    '',
+    'HAVE... HAVE YOU',
+    'TRIED WEED?',
+    '- JACOB',
+    ''
+  ],
+  [
+    '',
+    'OOP JUST PICKED UP',
+    'A COW CATCHER...',
+    '- WREN',
+    ''
+  ],
+  [
+    '',
+    'POSEIDY ON MY',
+    'WEINY',
+    '- ED',
+    ''
+  ],
+  [
+    '',
+    'OH I JUST SAW',
+    'YOUR PENIS',
+    '- WREN',
+    ''
+  ],
+  [
+    'IM COMING OVER TO',
+    'YOUR HOUSE TO',
+    'MOLEST THE BEE HIVE',
+    '- WREN',
+    ''
+  ],
+  [
+    'IN THE RESTROOM...',
+    'WE ARE ALL',
+    'MONSTERS.',
+    '- JACOB',
+    ''
+  ],
+  [
+    'THE THINGS PEOPLE DO',
+    'WHEN WAITING FOR',
+    'POOP TO DROP',
+    '- BEN',
+    ''
+  ],
+  [
+    '',
+    'MAYBE I SHOULD JUST',
+    'GET ANOTHER ASS',
+    '- WREN',
+    ''
+  ],
+  [
+    'I GET WHY WREN',
+    'LIKES THIS MOVIE,',
+    'ITS ABOUT A DEPRESSED',
+    'GUY WHO LOSES IT ALL',
+    '- EDDIE'
+  ],
+  [
+    '',
+    'I LOVE TINY',
+    'SAUSAGES',
+    '- JACOB',
+    ''
+  ],
+  [
+    '',
+    'THATS UH...',
+    'THATS HUMOUR',
+    '- JACOB',
+    ''
+  ],
+  [
+    '',
+    'IM NOT JOHNNY',
+    'BIG COCK',
+    '- JACOB',
+    ''
+  ],
+  [
+    'I WANT BRUCE CAMPBELL',
+    'TO GIVE ME MY',
+    'WEDDING RING',
+    '- EDDIE',
+    ''
+  ],
+  [
+    '',
+    'JUST GOT A FANTASTIC',
+    'VIEW OF WREN',
+    '- ED',
+    ''
+  ],
+  [
+    'HE WOULDNT HAVE ANY',
+    'MEAT BETWEEN HIS',
+    'BUNS',
+    '- WREN',
+    ''
+  ],
+  [
+    '',
+    'IM GONNA',
+    'MILK YOU.',
+    '- WREN',
+    ''
+  ],
+  [
+    '',
+    'OH SHIT',
+    'THATS A HORSE',
+    '- BEN',
+    ''
+  ],
+  [
+    '',
+    'WHAT DOES MY',
+    'NUT ABILITY DO?',
+    '- EDDIE',
+    ''
+  ],
+  [
+    '',
+    'I DIDNT EVEN MEAN',
+    'TO HAVE A GOOD IDEA',
+    '- EDDIE',
+    ''
+  ],
+  [
+    '',
+    'IVE JUST UNLOCKED',
+    'THE RED PICKLE',
+    '- ED',
+    ''
+  ],
+  [
+    'YOU MIGHT WANT TO',
+    'GET A NEW FORK.',
+    'DOGS AND CATS COME',
+    'ON THIS TABLE.',
+    '- WREN'
+  ],
+  [
+    '',
+    'IT WAS A GOOD DAY',
+    'FOR A FUNERAL.',
+    '- WREN',
+    ''
+  ],
+  [
+    '',
+    'A PHYSICAL COPY',
+    'OF CROCS',
+    '- JACOB',
+    ''
+  ],
+  [
+    '',
+    'DAISY YOU',
+    'MOTHERFUCKER!!!',
+    '- JACOB',
+    ''
+  ],
+  [
+    '',
+    'SUCK ME!',
+    'SUCK ME!!!',
+    '- BEN',
+    ''
+  ],
+  [
+    '',
+    'AND MY WOOD',
+    'IS DRY SO.',
+    '- WREN',
+    ''
+  ],
+  [
+    '',
+    'TELL MY WIFE',
+    'I LOVE HIM',
+    '- ED',
+    ''
+  ],
+  [
+    'I JUST WANNA PUT',
+    'THAT THICK LOG',
+    'IN THERE',
+    '- WREN',
+    ''
+  ],
+  [
+    '',
+    'JOHNSON, THE',
+    'INVENTOR OF SON?',
+    '- JACOB',
+    ''
+  ],
+  [
+    '',
+    'PUT SOME CHEST',
+    'ON HIS HAIRS',
+    '- WREN',
+    ''
+  ],
+  [
+    '',
+    'THERES A LOT OF',
+    'WOMEN I HATE',
+    '- EDDIE',
+    ''
+  ],
+  [
+    'TURNS OUT WE WERE',
+    'THE TWINKS ALL ALONG.',
+    'DAMN I JUST GOT',
+    'DONKED BY DICKS.',
+    '- EDDIE'
+  ],
+  [
+    '',
+    'YOU ARE AN ANT',
+    'BENEATH MY BOOT!!!',
+    '- JACOB',
+    ''
+  ],
+  [
+    'I THINK MY COMMENTS',
+    'ARE SUFFICEIST',
+    'TO SAY',
+    '- WREN',
+    ''
+  ],
+  [
+    '',
+    'LIKE I SAID BEFORE-',
+    'WHAT DID I SAY?',
+    '- EDDIE',
+    ''
+  ],
+  [
+    'ROADKILL BUTTHOLE',
+    'IS THE NAME OF MY',
+    'NEW ROCK BAND.',
+    '- JACOB',
+    ''
+  ],
+  [
+    '',
+    'I LITERALLY HAD',
+    'A DREAM WITH YOU',
+    '- JACOB',
+    ''
+  ],
+  [
+    '',
+    'OUR ASS IS',
+    'BACK',
+    '- JACOB',
+    ''
+  ],
+  [
+    '',
+    'NAAAIL IT TO',
+    'HIS HEEADDD',
+    '- JACOB',
     ''
   ]
 ];
